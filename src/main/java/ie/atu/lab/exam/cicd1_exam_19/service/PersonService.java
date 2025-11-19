8ccd9d958744880856f398c1a285e6c38e5b0ff3
JAMESDONNELLAN-STUDE <G00434671@atu.ie>
2025-11-19 14:19:35 +0000

    Set up Initial Person Service

--- src/main/java/ie/atu/lab/exam/cicd1_exam_19/service/PersonService.java
+++ src/main/java/ie/atu/lab/exam/cicd1_exam_19/service/PersonService.java
@@ -1,4 +1,32 @@
 package ie.atu.lab.exam.cicd1_exam_19.service;
 
-public class PersonService {
+import ie.atu.lab.exam.cicd1_exam_19.model.Person;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+
+import static org.springframework.util.ClassUtils.isPresent;
+
+@Service
+public class PersonService
+{
+    private final List<Person> store = new ArrayList<>();
+
+    public List<Person> findAll()
+    {
+        return new  ArrayList<>(store);
+    }
+    public Optional<Person> findById(String id)
+    {
+        for (Person p : store)
+        {
+           if (p.getPersonId().equals(id))
+           {
+               return Optional.of(p);
+           }
+        }
+        return Optional.empty();
+    }
 }
