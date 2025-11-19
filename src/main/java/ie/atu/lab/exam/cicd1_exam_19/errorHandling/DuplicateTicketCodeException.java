b61c9e8fc344d525946aef7ec104314cc5deb56a
JAMESDONNELLAN-STUDE <G00434671@atu.ie>
2025-11-19 15:08:49 +0000

    Added exception handlers for requested scenarios

--- src/main/java/ie/atu/lab/exam/cicd1_exam_19/errorHandling/DuplicateTicketCodeException.java
+++ src/main/java/ie/atu/lab/exam/cicd1_exam_19/errorHandling/DuplicateTicketCodeException.java
@@ -1,7 +1,9 @@
 package ie.atu.lab.exam.cicd1_exam_19.errorHandling;
 
-public class DuplicateTicketCodeException extends RuntimeException {
-    public DuplicateTicketCodeException(String message) {
-        super(message);
+public class DuplicateTicketCodeException extends RuntimeException
+{
+    public DuplicateTicketCodeException(String message)
+    {
+        super(message = "[ERROR] Duplicate Ticket Code.");
     }
 }
