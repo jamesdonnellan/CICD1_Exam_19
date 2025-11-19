534368bdde75dc59fd26eb4e233d721df8c58f32
JAMESDONNELLAN-STUDE <G00434671@atu.ie>
2025-11-19 15:01:55 +0000

    Added Exception Details variables to fix bug in GEH

--- src/main/java/ie/atu/lab/exam/cicd1_exam_19/errorHandling/ExceptionDetails.java
+++ src/main/java/ie/atu/lab/exam/cicd1_exam_19/errorHandling/ExceptionDetails.java
@@ -1,4 +1,14 @@
 package ie.atu.lab.exam.cicd1_exam_19.errorHandling;
 
-public class ExceptionDetails {
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class ExceptionDetails
+{
+    private String fieldName;
+    private String fieldValue;
 }
