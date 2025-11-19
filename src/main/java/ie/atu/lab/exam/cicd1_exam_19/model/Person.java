7f605ce3498e217b921cf4247e1097010f13d90a
JAMESDONNELLAN-STUDE <G00434671@atu.ie>
2025-11-19 14:10:43 +0000

    Set up Person Model with correct fiels and validation

--- src/main/java/ie/atu/lab/exam/cicd1_exam_19/model/Person.java
+++ src/main/java/ie/atu/lab/exam/cicd1_exam_19/model/Person.java
@@ -1,4 +1,27 @@
 package ie.atu.lab.exam.cicd1_exam_19.model;
 
-public class Person {
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
+import jakarta.validation.constraints.Positive;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor @AllArgsConstructor @Builder
+public class Person
+{
+    @NotBlank
+    private String attendeeName;
+
+    @Email
+    private String email;
+
+    @Pattern(regexp = "TK-[0-9]{4}")
+    private String ticketCode;
+
+    @Positive
+    private int quantity;
 }
