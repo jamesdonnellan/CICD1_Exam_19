529632e3ead4c31061e07d319175e74e8c227aaa
JAMESDONNELLAN-STUDE <G00434671@atu.ie>
2025-11-19 15:05:17 +0000

    Small error fixes in GEH

--- src/main/java/ie/atu/lab/exam/cicd1_exam_19/errorHandling/GlobalErrorHandling.java
+++ src/main/java/ie/atu/lab/exam/cicd1_exam_19/errorHandling/GlobalErrorHandling.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
 import java.util.ArrayList;
-import java.util.List
+import java.util.List;
 
 @ControllerAdvice
 public class GlobalErrorHandling
