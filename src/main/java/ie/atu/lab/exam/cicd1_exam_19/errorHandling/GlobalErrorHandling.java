1a81a17e71ddd7c722328d95d32dcffe1a6838df
JAMESDONNELLAN-STUDE <G00434671@atu.ie>
2025-11-19 14:59:37 +0000

    Current GEH progress

--- src/main/java/ie/atu/lab/exam/cicd1_exam_19/errorHandling/GlobalErrorHandling.java
+++ src/main/java/ie/atu/lab/exam/cicd1_exam_19/errorHandling/GlobalErrorHandling.java
@@ -1,6 +1,28 @@
 package ie.atu.lab.exam.cicd1_exam_19.errorHandling;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+import java.util.ArrayList;
+
+@ControllerAdvice
 public class GlobalErrorHandling
 {
-
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<List<ExceptionDetails>> showErrorDetails(MethodArgumentNotValidException mae)
+    {
+        List<ExceptionDetails> errorList = new ArrayList<>();
+        for (FieldError fieldError : mae.getBindingResult().getFieldErrors())
+        {
+            ExceptionDetails exceptionDetails = new ExceptionDetails();
+            exceptionDetails.setFieldName(fieldError.getField());
+            exceptionDetails.setFieldValue(fieldError.getDefaultMessage());
+            errorList.add(ExceptionDetails);
+        }
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(errorList);
+    }
 }
