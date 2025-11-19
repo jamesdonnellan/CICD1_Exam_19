a0d30e9901e46f088f79c5d2609dc06d77f75e44
JAMESDONNELLAN-STUDE <G00434671@atu.ie>
2025-11-19 14:06:07 +0000

    Set up project folders and files

--- src/main/java/ie/atu/lab/exam/cicd1_exam_19/Cicd1Exam19Application.java
+++ src/main/java/ie/atu/lab/exam/cicd1_exam_19/Cicd1Exam19Application.java
@@ -6,7 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class Cicd1Exam19Application {
 
-    public static void main(String[] args) {
+    public static void main(String[] args)
+    {
         SpringApplication.run(Cicd1Exam19Application.class, args);
     }
 
